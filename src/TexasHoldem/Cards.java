72c2b584a6ad3ee02af7faf56e9a4764135fc518
Jameson Knott <jsktrogdor@gmail.com>
2019-07-17 23:22:11 -0600

    Built card class. Intialized deck ArrayList in main method.

--- src/TexasHoldem/Cards.java
+++ src/TexasHoldem/Cards.java
@@ -2,48 +2,119 @@ package TexasHoldem;
 
 public class Cards
 {
-    //Four, Five, King, etc
-    private String cardName;
 
-    //Clubs, Diamonds, Hearts, etc
-    private String cardSuit;
+    private final Names cardName;
+    private final Suits cardSuit;
 
-    //jack = 11, queen = 12, ace = 1 || 11
-    private int cardWeight;
+    private Cards(Names cardName, Suits cardSuit){
+        this.cardName = cardName;
+        this.cardSuit = cardSuit;
 
-    public void Cards(String name, String suit, int weight){
-        setCardName(name);
-        setCardSuit(suit);
-        setCardWeight(weight);
-    }
-
-    public String getCardName() {
-        return cardName;
-    }
-
-    public void setCardName(String name){
-        cardName = name;
-    }
-
-    public String getCardSuit(){
-        return cardSuit;
-    }
-
-    public void setCardSuit(String suit){
-        cardSuit = suit;
-    }
-
-    public int getCardWeight(){
-        return cardWeight;
-    }
-
-    public void setCardWeight(int weight){
-        cardWeight = weight;
     }
 
 
+    public enum Names
+    {
+        TWO(2, "2", "Two"),
+        THREE(3, "3", "Three"),
+        FOUR(4, "4", "Four"),
+        FIVE(5, "5", "Five"),
+        SIX(6, "6", "Six"),
+        SEVEN(7, "7", "Seven"),
+        EIGHT(8, "8", "Eight"),
+        NINE(9, "9", "Nine"),
+        TEN(10, "10", "Nine"),
+        JACK(11, "J", "Jack"),
+        QUEEN(12, "Q", "Queen"),
+        KING(13, "K", "King"),
+        ACE(14, "A", "Ace");
 
-    /*Make an ArrayList so that cards can be added
-    and removed
+        private int cardWeight;
+        private String cardSymbol;
+        private String cardText;
+
+        Names(int weight, String symbol, String text)
+        {
+            cardWeight = weight;
+            cardSymbol = symbol;
+            cardText = text;
+        }
+    }
+    /*
+    Enumeration list containing all card names (2 - Ace).
+
+    Each Enum Constant Includes:
+    --------------------------------------------------------------
+    int weight      :   The value the card carries in the game.
+    String symbol   :   The symbol of the card name (String because "10").
+    String text     :   A text string of the card's name.
      */
+
+
+    public enum Suits {HEARTS('\u2665', "Hearts"),
+        DIAMONDS('\u2666', "Diamonds"),
+        CLUBS('\u2663', "Clubs"),
+        SPADES('\u2660', "Spades");
+
+        private char suitSymbol;
+        private String suitText;
+
+        Suits(char symbol, String text) {
+            suitSymbol = symbol;
+            suitText = text;
+        }
+    }
+    /*
+    Enumeration list containing all card suits (Hearts, Diamonds, etc).
+
+    Each Enum Constant Includes:
+    ---------------------------------------------------------------
+    char symbol :   A unicode character code for the suit symbol.
+    String text :   A text string of the suit's name.
+     */
+
+
+
+    /*
+    Override toString which prints each Card class object called as:
+
+    | n s |
+
+    Where n = cardSymbol
+          s = suitSymbol
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("| %s %c |", cardName.cardSymbol, cardSuit.suitSymbol);
+    }
+
+
+
+    /*
+    Various getters.  I'm not sure they're needed anymore with the Enum Lists
+     */
+//    public String getCardNameString(){
+//        return cardName.cardText;
+//    }
+//
+//    public String getCardSuitString(){
+//        return cardSuit.suitText;
+//    }
+//
+//    public String getCardNameSymbol(){
+//        return cardName.cardSymbol;
+//    }
+//
+//    public char getCardSuitSymbol(){
+//        return cardSuit.suitSymbol;
+//    }
+//
+//    public int getCardWeight(){
+//        return cardName.cardWeight;
+//    }
+
+
 }
+
+
--- src/TexasHoldem/Main.java
+++ src/TexasHoldem/Main.java
@@ -1,5 +1,7 @@
 package TexasHoldem;
 
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args){
@@ -9,6 +11,32 @@ public class Main
 
         Dealer.getPot();
         Dealer.setPot(50);
+
+
+        /*
+        Deck ArrayList initialization.
+        A set of nested for each loops that use the index values of the
+        Cards.Names and Cards.Suits enums to create all of our cards.
+         */
+        ArrayList<Cards> deck = new ArrayList<>();
+        int i = 0;
+        for(Cards.Suits suits : Cards.Suits.values()) {
+
+            int j = 0;
+
+            for (Cards.Names names : Cards.Names.values()) {
+
+                deck.add(new Cards(Cards.Names.values()[j],
+                        Cards.Suits.values()[i]));
+                j++;
+            }
+
+            i++;
+        }
+
+
+        //Prints the entire deck.
+        System.out.print(deck);
 
     }
 }
